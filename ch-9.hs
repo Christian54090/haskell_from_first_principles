0414f52db4b058db6d335b5e429fdbdc3652e658
Christian54090 <christian.sanders@live.com>
2019-12-20 19:28:11 -0800

    Lists review

--- ch-9.hs
+++ ch-9.hs
@@ -30,3 +30,19 @@ eftChar a b
   | a > b     = [a] ++ eftChar (pred a) b
   | a < b     = [a] ++ eftChar (succ a) b
   | otherwise = [a]
+
+multOf3 :: (Eq a, Integral a) => [a] -> [a]
+multOf3 a = filter (\x -> (rem x 3) == 0) a
+
+exceptArticles :: String -> [String]
+exceptArticles a = filter (\w -> w /= "the" && w /= "a") $ words a
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
