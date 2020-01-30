fcc82ac6b6227dac94ea2d6be2120fbce77236c6
Christian54090 <christian.sanders@live.com>
2020-01-30 12:40:51 -0800

    Recursion p2

---
+++ ch-8-1.hs
@@ -0,0 +1,16 @@
+factorial :: Integer -> Integer
+factorial 1 = 1
+factorial n = n * factorial (n-1)
+
+fibonacci :: Integral a => a -> a
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n-1) + fibonacci (n-2)
+
+sumAll :: (Eq a, Num a) => a -> a
+sumAll 1 = 1
+sumAll n = n + sumAll (n-1)
+
+summation :: Integral a => a -> a -> a
+summation n 1 = n
+summation n t = n + summation n (t-1)
