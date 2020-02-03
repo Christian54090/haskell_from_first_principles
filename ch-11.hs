4429a2f460279c0424a290ca73fc81c0e3245bae
Christian54090 <christian.sanders@live.com>
2020-02-03 12:50:59 -0800

    Algebraic Datatypes p2

--- ch-11.hs
+++ ch-11.hs
@@ -42,7 +42,66 @@ getManu (Car m _) = m
 
 isCar' :: Vehicle -> Bool
 isCar' (Car _ _) = True
-isCar' (Plane _) = False
+isCar' _         = False
 
 isPlane' :: Vehicle -> Bool
 isPlane' = not . isCar'
+
+areCars' :: [Vehicle] -> [Bool]
+areCars' = map isCar'
+
+data Person =
+  Person { name :: String
+         , age :: Int}
+         deriving (Eq, Show)
+
+jm = Person "julie" 108
+ca = Person "chris" 16
+
+data List a = Nil | Cons a (List a)
+
+data BinaryTree a =
+    Leaf
+  | Node (BinaryTree a) a (BinaryTree a)
+  deriving (Eq, Ord, Show)
+
+insert' :: Ord a => a -> BinaryTree a -> BinaryTree a
+insert' v Leaf = Node Leaf v Leaf
+insert' v (Node left v' right)
+  | v < v'  = Node (insert' v left) v' right
+  | v > v'  = Node left v' (insert' v right)
+  | v == v' = Node left v right
+-- recursively inserts until it finds an empty slot,
+-- at which it inserts the base case (Node Leaf v Leaf) into the tree
+
+mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+mapTree _ Leaf = Leaf
+mapTree f (Node left a right) = Node (mapTree f left) (f a) (mapTree f right)
+
+testTree' :: BinaryTree Integer
+testTree' =
+  Node (Node Leaf 3 Leaf)
+  1
+       (Node Leaf 4 Leaf)
+
+mapExpected =
+  Node (Node Leaf 4 Leaf)
+  2
+       (Node Leaf 5 Leaf)
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf                = []
+inorder (Node left a right) = (inorder left) ++ [a] ++ (inorder right)
+
+testTree :: BinaryTree Integer
+testTree =
+  Node (Node Leaf 1 Leaf)
+  2
+       (Node Leaf 3 Leaf)
+
+bstSearch :: (Eq a, Ord a) => a -> BinaryTree a -> Bool
+bstSearch v Leaf = False
+bstSearch v (Node left a right)
+  | v < a  = bstSearch v left
+  | v > a  = bstSearch v right
+  | v == a = True
