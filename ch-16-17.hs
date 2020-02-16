7d108463b42ad95be74b1ff052ae012a962130a5
Christian54090 <christian.sanders@live.com>
2020-02-15 21:52:11 -0800

    Applicative p2

--- ch-16-17.hs
+++ ch-16-17.hs
@@ -1,3 +1,7 @@
+import Control.Applicative
+import Data.Monoid
+import Data.List (elemIndex)
+
 -- Functors are things that can be mapped over (lists, maybe, trees, etc)
 -- Functor laws:
 -- Identity -
@@ -44,3 +48,99 @@ instance Functor CMaybe where
 listAppEx = [(+1), (*2)] <*> [2,4]
 -- -> [ (+1) 2, (+1) 4, (*2) 2, (*2) 4 ]
 -- => [3,5,4,8]
+
+-- fmap f x == pure f <*> x
+
+app :: Applicative f => f (a -> b) -> f a -> f b
+app = (<*>)
+-- f (a -> b) => 1st argument is a structure that holds a functor
+-- f a => 2nd argument is a structure that holds an element
+
+data Optional a = Nada | Yep a deriving (Show)
+
+instance Functor Optional where
+  fmap f Nada    = Nada
+  fmap f (Yep a) = Yep (f a)
+
+instance Applicative Optional where
+  pure                = Yep
+  Nada <*> _          = Nada
+  _ <*> Nada          = Nada
+  (Yep f) <*> (Yep a) = Yep (f a)
+
+tupleApp = (,) <$> [1,2] <*> [3,4]
+tupleApp' = liftA2 (,) [1,2] [3,4]
+tupleApp'' = liftA3 (,,) [1,2] [3,4] [5,6]
+
+added :: Maybe Integer
+added = (+3) <$> (lookup 3 $ zip [1,2,3] [4,5,6])
+
+y' :: Maybe Integer
+y' = lookup 3 $ zip [1,2,3] [4,5,6]
+
+z' :: Maybe Integer
+z' = lookup 2 $ zip [1,2,3] [4,5,6]
+
+tupled :: Maybe (Integer, Integer)
+tupled = (,) <$> y' <*> z'
+
+newtype Identity a = Identity a deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+instance Applicative Identity where
+  pure = Identity
+  (Identity f) <*> (Identity a) = Identity (f a)
+
+newtype Constant a b = Constant { getConstant :: a } deriving (Eq, Ord, Show)
+
+instance Functor (Constant a) where
+  fmap _ (Constant a) = Constant a
+
+instance Monoid a => Applicative (Constant a) where
+  pure _ = Constant mempty
+  (Constant f) <*> (Constant a) = Constant $ f <> a
+
+data List a = Nil | Cons a (List a) deriving (Eq, Show)
+
+instance Functor List where
+  fmap _ Nil         = Nil
+  fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+
+instance Applicative List where
+  pure a = Cons a Nil
+  Nil <*> _ = Nil
+  _ <*> Nil = Nil
+  (Cons f fs) <*> xs = (f <$> xs) `append` (fs <*> xs)
+
+append :: List a -> List a -> List a
+append Nil ys = ys
+append (Cons x xs) ys = Cons x $ append xs ys
+
+data Four a b c d = Four a b c d
+
+instance Functor (Four a b c) where
+  fmap f (Four a b c d) = Four a b c (f d)
+
+instance (Monoid a, Monoid b, Monoid c) => Applicative (Four a b c) where
+  pure = Four mempty mempty mempty
+  (Four a b c d) <*> (Four w x y z) = Four (a <> w) (b <> x) (c <> y) (d z)
+
+data Four' a b = Four' a a a b
+
+instance Functor (Four' a) where
+  fmap f (Four' a b c d) = Four' a b c (f d)
+
+instance Monoid a => Applicative (Four' a) where
+  pure = Four' mempty mempty mempty
+  (Four' a b c d) <*> (Four' w x y z) = Four' (a <> w) (b <> x) (c <> y) (d z)
+
+stops :: String
+stops = "pbtdkg"
+
+vowels :: String
+vowels = "aeiou"
+
+combos :: [a] -> [b] -> [c] -> [(a,b,c)]
+combos = liftA3 (,,)
